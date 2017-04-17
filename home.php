b216681ecc8730d21edf06b3f558f9710df00407
Elton <19.elton@gmail.com>
2017-04-16 23:33:10 -0300

    criei a pagina home da area restrita e adcionei fotos

---
+++ home.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="br">
+<head>
+  <meta charset="UTF-8">
+  <title>Integrador-jonadab/Elton</title>
+  <link rel="stylesheet" href="css/gallery.prefixed.css">
+ <!-- <link rel="stylesheet" href="css/gallery.theme.css">-->
+  <?php
+  require_once ('nav.php');
+  ?>
+</head>
+<body>
+<div class="wrapper">
+<?php
+require_once ('boot.php');
+?>
+<div class="container">
+
+<article class="row">
+
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 row">
+		<div  class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+		 	<a href="" class = "">
+		 		<img class="img-responsive col-xs-12 col-sm-12 col-md-12 col-lg-12 " src="images/relatorio.jpg">
+		 	</a>
+		 	<h2 class="news text-center">Relatórios</h2>
+		</div>
+
+		<div  class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ">
+	 		<a href="" class = "">
+	 			<img class="img-responsive col-xs-12 col-sm-12 col-md-12 col-lg-12" src="images/venda.jpg"> 
+	 		</a>
+	 		<h2 class="news text-center">Venda</h2>
+	 	</div>
+	</div>
+
+
+
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 row">
+		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+			<a href="" class = "">
+				 <img class="img-responsive col-xs-12 col-sm-12 col-md-12 col-lg-12 " src="images/consulta.jpg">
+			</a>
+			<h2 class="news text-center">Consulta</h2>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+			<a href="" class = "">
+	 			<img class="img-responsive col-xs-12 col-sm-12 col-md-12 col-lg-12 " src="images/apontamento.jpg"> 
+	 		</a>
+	 		<h2 class="news text-center">Apontamento</h2>
+	 	</div>
+	 </div>
+
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 row">
+
+		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+	 		<a href="" class = "">
+	 			<img class="img-responsive col-xs-12 col-sm-12 col-md-12 col-lg-12 " src="images/operações.jpg"> 
+	 		</a>
+	 		<h2 class="news text-center">Operações</h2>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+	 		<a href="" class = "">
+	 			<img class="img-responsive col-xs-12 col-sm-12 col-md-12 col-lg-12 " src="images/cadastro.jpg"> 
+	 		</a>
+	 		<h2 class="news text-center">Cadrastro</h2>
+		</div>
+	</div>	 
+</article>
+
+</div>
+<?php
+require_once ('footer.php');
+?>
+</div>
+
+</body>
+</html>
